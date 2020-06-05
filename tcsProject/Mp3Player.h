e98d5c0fb6031407aa8035fbab14817352c72899
plasma15 <momentum15@naver.com>
2020-06-05 23:52:03 +0900

    Added detail playing features and 'focus' command for software-serial

--- tcsProject/Mp3Player.h
+++ tcsProject/Mp3Player.h
@@ -1,10 +1,13 @@
 #include "DFPlayer_Mini_Mp3.h"
 #define VOL 20
+
 class SoundPlayer {
   public:
     SoundPlayer();
     void playSound(const byte& id) const;
-    void set();
+    void set() const;
+    void playErrorSound(const byte &code)const;
+    void playStop(const byte &code)const;
 
   private:
     SoftwareSerial* mySerial;
@@ -14,16 +17,25 @@ SoundPlayer::SoundPlayer() {
   this->mySerial = new SoftwareSerial(4, 5);
 }
 
-
-void SoundPlayer::set() {
-  Serial.println("TEST");
+void SoundPlayer::set()const  {
   mySerial->begin(9600);
   mp3_set_serial((*mySerial));
   delay(1);
   mp3_set_volume(VOL);
 }
 
+void SoundPlayer::playErrorSound(const byte &code)const {
+  mySerial->listen();
+  mp3_play(code);
+}
+
+void SoundPlayer::playStop(const byte &code)const {
+  mySerial->listen();
+  mp3_play(code);
+}
+
 void SoundPlayer:: playSound(const byte& id) const {
+  mySerial->listen();
   mp3_play(id);
-  delay(3000);
+  delay(2500);
 }
